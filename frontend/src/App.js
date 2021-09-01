c292bb8acef8e343cffa2fcc2c53891a0492e784
Divya <divyambadala43@gmail.com>
2021-09-01 14:09:21 +0530

    Add: ChannelContainer and ChannelListContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelContainer, ChannelListContainer } from "./components";
+import './App.css'
+
+const apiKey = "n4nf4we7669t";
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Realtime Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme='team light'>
+          <ChannelListContainer />
+          <ChannelContainer />
+      </Chat>
     </div>
   );
 };
